21040ee0471a370934bc30edc7b8aeb7ce9215bd
alankarpandey <alankarlinux02@gmail.com>
2025-03-12 18:58:11 +0530

    clientt

--- google_ads/clinet.py
+++ google_ads/clinet.py
@@ -3,13 +3,13 @@ from google.ads.googleads.client import GoogleAdsClient
 
 def get_client():
     try:
-        client = GoogleAdsClient.load_from_storage(
-            r"C:\Users\nickc\OneDrive\Desktop\AI marketing\google_ads\google-ads.yaml"
-        )
         # client = GoogleAdsClient.load_from_storage(
-        #     "/home/ubuntu/ai_marketing/google_ads/google-ads.yaml",
-        #     version="v18"
+        #     r"C:\Users\nickc\OneDrive\Desktop\AI marketing\google_ads\google-ads.yaml"
         # )
+        client = GoogleAdsClient.load_from_storage(
+            "/home/ubuntu/ai_marketing/google_ads/google-ads.yaml",
+            version="v18"
+        )
         return client
     except Exception as e:
         print(f"Error in get_client: {e}")
